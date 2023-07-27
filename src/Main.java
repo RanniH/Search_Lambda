f2d478223d060b130f1157ddf0927e8fe1077b87
RanniS <1752092407@qq.com>
2023-07-27 22:14:19 +0800

    third commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("haha");
         System.out.println("moximoxi");
+        System.out.println("pushTest");
     }
 }
